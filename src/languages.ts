5481e4d3073dcf91de279e85742c6d047331ccb0
remin <noreply@remin.jp>
2019-09-07 02:03:08 +0900

    add ccn analysis

--- src/languages.ts
+++ src/languages.ts
@@ -1,19 +1,62 @@
 import { CLexer, CParser } from './grammars/C';
 import { CPP14Lexer, CPP14Parser } from './grammars/CPP14';
 
-export default [
+const languages: LanguageConfig[] = [
   {
     test: /\.c$/,
     name: 'c',
     lexar: CLexer,
     parser: CParser,
-    rules: { root: 'primaryExpression' },
+    rules: {
+      root: [{ rule: 'compilationUnit' }],
+      functionDefinition: [{ rule: 'functionDefinition' }],
+      ccnIncrement: [
+        { token: 'if' },
+        { token: 'while' },
+        { token: 'case' },
+        { token: '&&' },
+        { token: '||' },
+        { token: '?' },
+      ],
+    },
   },
   {
     test: /\.cpp$/,
     name: 'cpp',
     lexar: CPP14Lexer,
     parser: CPP14Parser,
-    rules: { root: 'translationunit' },
+    rules: {
+      root: [{ rule: 'translationunit' }],
+      functionDefinition: [{ rule: 'functiondefinition' }],
+      ccnIncrement: [
+        { token: 'if' },
+        { token: 'while' },
+        { token: 'case' },
+        { token: '&&' },
+        { token: 'and' },
+        { token: '||' },
+        { token: 'or' },
+        { token: '?' },
+      ],
+    },
   },
 ];
+
+interface LanguageConfig {
+  test: RegExp;
+  name: string;
+  lexar: any;
+  parser: any;
+  rules: {
+    root: Rule[];
+    functionDefinition: Rule[];
+    ccnIncrement: Rule[];
+  };
+}
+
+interface Rule {
+  rule?: string;
+  token?: string;
+}
+
+export default languages;
