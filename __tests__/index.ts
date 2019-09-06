15522c3d750900897346f64c2ca933bf64df5766
remin <rdrgn@users.noreply.github.com>
2019-09-06 19:52:42 +0900

    add test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,5 +1,35 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { analyze } from '../dist';
 
-test('test', () => {
-  analyze('test.cpp', 'int main(){}');
+const testCasePath = path.resolve(__dirname, 'cases');
+const inputFileNameRegExp = /^input\.[^.]+$/;
+const outputFileNameRegExp = /^output.json$/;
+
+describe('analyze', () => {
+  const dirs = fs.readdirSync(testCasePath, { withFileTypes: true });
+
+  dirs.forEach(dir => {
+    if (!dir.isDirectory()) {
+      return;
+    }
+
+    const files = fs.readdirSync(path.resolve(testCasePath, dir.name), { withFileTypes: true });
+
+    const inputFile = files.find(f => f.isFile() && inputFileNameRegExp.test(f.name));
+    const outputFile = files.find(f => f.isFile() && outputFileNameRegExp.test(f.name));
+
+    if (inputFile === undefined || outputFile === undefined) {
+      return;
+    }
+
+    const input = fs.readFileSync(path.resolve(testCasePath, dir.name, inputFile.name), { encoding: 'utf-8' });
+    const output = fs.readFileSync(path.resolve(testCasePath, dir.name, outputFile.name), { encoding: 'utf-8' });
+    const outputObject = JSON.parse(output);
+
+    test(`case '${dir.name}'`, async done => {
+      expect(await analyze(inputFile.name, input)).toEqual(outputObject);
+      done();
+    });
+  });
 });
