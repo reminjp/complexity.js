e9d1e5a5de1048aecc18fb22efcc423d11517c63
remin <noreply@remin.jp>
2019-09-07 04:22:09 +0900

    update antlr4.js to separate src/dest dir

--- antlr4.js
+++ antlr4.js
@@ -3,25 +3,30 @@ const fs = require('fs');
 const path = require('path');
 
 const antlrCommand = 'antlr';
-const grammarPath = path.resolve(__dirname, 'src', 'grammars');
+const sourcePath = path.resolve(__dirname, 'res', 'grammars');
+const destiinationPath = path.resolve(__dirname, 'src', 'grammars');
 const grammarFileExtentionRegExp = /\.g4$/;
 const javaScriptFileExtentionRegExp = /\.js$/;
 
+// run ANTLR4
 const grammarFiles = fs
-  .readdirSync(grammarPath, { withFileTypes: true })
+  .readdirSync(sourcePath, { withFileTypes: true })
   .filter(f => f.isFile() && grammarFileExtentionRegExp.test(f.name));
 
 grammarFiles.forEach(file => {
-  child_process.execSync(`${antlrCommand} -Dlanguage=JavaScript -no-listener ${path.resolve(grammarPath, file.name)}`);
+  child_process.execSync(
+    `${antlrCommand} -o ${destiinationPath} -Dlanguage=JavaScript -no-listener ${path.resolve(sourcePath, file.name)}`
+  );
 });
 
 const javaScriptFiles = fs
-  .readdirSync(grammarPath, { withFileTypes: true })
+  .readdirSync(destiinationPath, { withFileTypes: true })
   .filter(f => f.isFile() && javaScriptFileExtentionRegExp.test(f.name));
 const classNames = javaScriptFiles.map(f => f.name.replace(javaScriptFileExtentionRegExp, ''));
 
+// create .ts files
 classNames.forEach(className => {
-  fs.writeFile(path.resolve(grammarPath, `${className}.d.ts`), `export const ${className}: any;\n`, error => {
+  fs.writeFile(path.resolve(destiinationPath, `${className}.d.ts`), `export const ${className}: any;\n`, error => {
     if (error) {
       console.log(error);
     }
@@ -29,7 +34,7 @@ classNames.forEach(className => {
 });
 
 fs.writeFile(
-  path.resolve(grammarPath, 'index.ts'),
+  path.resolve(destiinationPath, 'index.ts'),
   classNames.map(c => `export * from './${c}';\n`).join(''),
   error => {
     if (error) {
