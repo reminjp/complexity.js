d356b27fe8e14efda75bb86e9dc4c55b557cc675
remin <rdrgn@users.noreply.github.com>
2019-09-09 03:35:57 +0900

    update cpp test case

--- __tests__/cases/cpp/input.cpp
+++ __tests__/cases/cpp/input.cpp
@@ -7,3 +7,17 @@ void function(bool condition1, bool condition2) {
     std::cout << "Hello, World!" << std::endl;
   }
 }
+
+void fizzbuzz(int n) {
+  for (int i = 1; i < n; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      std::cout << "FizzBuzz" << std::endl;
+    } else if (i % 3 == 0) {
+      std::cout << "Fizz" << std::endl;
+    } else if (i % 5 == 0) {
+      std::cout << "Buzz" << std::endl;
+    } else {
+      std::cout << n << std::endl;
+    }
+  }
+}
