58941d542ce82f4cd2658229ac447f6d86ba4594
remin <noreply@remin.jp>
2019-09-13 17:45:08 +0900

    refactor test

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -6,29 +6,46 @@ const testCasePath = path.resolve(__dirname, 'cases');
 const inputFileNameRegExp = /^input\.[^.]+$/;
 const outputFileNameRegExp = /^output.json$/;
 
+interface TestCase {
+  name: string;
+  input: {
+    filename: string;
+    code: string;
+  };
+  output: any;
+}
+
+const testCases: TestCase[] = [];
+
+fs.readdirSync(testCasePath, { withFileTypes: true }).forEach(dir => {
+  if (!dir.isDirectory()) {
+    return;
+  }
+
+  const files = fs.readdirSync(path.resolve(testCasePath, dir.name), { withFileTypes: true });
+
+  const inputFile = files.find(f => f.isFile() && inputFileNameRegExp.test(f.name));
+  const outputFile = files.find(f => f.isFile() && outputFileNameRegExp.test(f.name));
+
+  if (inputFile === undefined || outputFile === undefined) {
+    return;
+  }
+
+  const input = fs.readFileSync(path.resolve(testCasePath, dir.name, inputFile.name), { encoding: 'utf-8' });
+  const output = fs.readFileSync(path.resolve(testCasePath, dir.name, outputFile.name), { encoding: 'utf-8' });
+  const outputObject = JSON.parse(output);
+
+  testCases.push({
+    name: `${dir.name}/${inputFile.name}`,
+    input: { filename: inputFile.name, code: input },
+    output: outputObject,
+  });
+});
+
 describe('analyze', () => {
-  const dirs = fs.readdirSync(testCasePath, { withFileTypes: true });
-
-  dirs.forEach(dir => {
-    if (!dir.isDirectory()) {
-      return;
-    }
-
-    const files = fs.readdirSync(path.resolve(testCasePath, dir.name), { withFileTypes: true });
-
-    const inputFile = files.find(f => f.isFile() && inputFileNameRegExp.test(f.name));
-    const outputFile = files.find(f => f.isFile() && outputFileNameRegExp.test(f.name));
-
-    if (inputFile === undefined || outputFile === undefined) {
-      return;
-    }
-
-    const input = fs.readFileSync(path.resolve(testCasePath, dir.name, inputFile.name), { encoding: 'utf-8' });
-    const output = fs.readFileSync(path.resolve(testCasePath, dir.name, outputFile.name), { encoding: 'utf-8' });
-    const outputObject = JSON.parse(output);
-
-    test(`${dir.name}/${inputFile.name}`, async done => {
-      expect(await analyze(inputFile.name, input)).toEqual(outputObject);
+  testCases.forEach(testCase => {
+    test(testCase.name, async done => {
+      expect(await analyze(testCase.input.filename, testCase.input.code)).toEqual(testCase.output);
       done();
     });
   });
